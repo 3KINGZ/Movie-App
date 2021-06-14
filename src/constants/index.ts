f998d1b1e152ea2ecd06ba1bf2ab522bb5176255
3kingz <victorkenneth95@gmail.com>
2021-06-14 13:09:35 +0100

    modifications

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,6 +1,3 @@
 export * from "./genres";
 export * from "./urls";
 export * from "./category";
-
-export const posterURL = "https://image.tmdb.org/t/p/w500/";
-export const backDropURL = "https://image.tmdb.org/t/p/w500/";
--- src/constants/urls.ts
+++ src/constants/urls.ts
@@ -3,3 +3,5 @@ export const searchURL = "https://api.themoviedb.org/3/search/movie";
 export const apiKey = "52050e6e3220743e0fba6b8a62e6eccf";
 export const other = "&language=en-US&page=1";
 export const genreURL = `https://api.themoviedb.org/3/discover/movie?api_key=${apiKey}`;
+export const posterURL = "https://image.tmdb.org/t/p/w500/";
+export const backDropURL = "https://image.tmdb.org/t/p/w500/";
