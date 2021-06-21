c88a042af166148d86336d1a7f3ce58538a3ec04
3kingz <victorkenneth95@gmail.com>
2021-06-21 09:30:33 +0100

    enable rn debugger

--- src/store.ts
+++ src/store.ts
@@ -3,6 +3,19 @@ import thunk from "redux-thunk";
 
 import rootReducer from "./reducers";
 
-const store = createStore(rootReducer, applyMiddleware(thunk));
+const composeEnhancers =
+  typeof window === "object" && window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__
+    ? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__({
+        thunk,
+        // Specify extension’s options like name, actionsBlacklist, actionsCreators, serialize...
+      })
+    : compose;
+
+const enhancer = composeEnhancers(
+  applyMiddleware(thunk),
+  // other store enhancers if any
+);
+
+const store = createStore(rootReducer, enhancer);
 
 export default store;
